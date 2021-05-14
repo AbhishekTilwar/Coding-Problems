446e90eff86f5d0536b65df1b17826a03909f2f5
Abhishek Tilwar <abhishektilwar@gmail.com>
2021-05-14 10:44:41 +0530

    Update Check Prime No.cpp

--- Check Prime No.cpp
+++ Check Prime No.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
 int main(){
     int b;
     cin>>b;
     int i;
-    for(i=2; i<b;i++){
-        if(b%i==0){
+    for(i=2; i<b;i++)
+    {
+        if(b%i==0)
+        {
             cout<<"Non-Prime"<<endl;
             break;
         }
     }
-        if(i==b){
+        if(i==b)
+        {
             cout<<"Prime"<<endl;
         }
     return 0;
