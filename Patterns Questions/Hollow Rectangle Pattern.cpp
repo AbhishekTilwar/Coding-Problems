bc883dd3dd73f62fc8cdc8b3db7bcf66e93e4c75
Abhishek Tilwar <abhishektilwar@gmail.com>
2021-05-16 00:13:25 +0530

    Create Hollow Rectangle Pattern.cpp

---
+++ Patterns Questions/Hollow Rectangle Pattern.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int row,col;
+    cin>>row>>col;
+     for(int j=0;j<col;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+    for(int i=0;i<(row+1)/2;i++)
+    {
+        cout<<"*";
+        for(int j=0;j<(col+1)/2;j++){
+            cout<<" ";
+        }
+        cout<<"*"<<endl;
+    }
+    
+    for(int j=0;j<col;j++){
+            cout<<"*";
+        }       
+        cout<<endl;
+
+
+}
