445b058ebb297a27e2e9488a468dbd773f48fc21
Abhishek Tilwar <abhishektilwar@gmail.com>
2021-05-16 01:00:10 +0530

    Create Half Pyramid.Pattern using Numbers.cpp

---
+++ Patterns Questions/Half Pyramid.Pattern using Numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+    int i;
+    for(i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++){
+            {
+                cout<<i;
+            }  
+        }
+        cout<<endl;
+    }
+}
+
+/* Pattern
+
+1
+22
+333
+4444
+55555
+
+*/
