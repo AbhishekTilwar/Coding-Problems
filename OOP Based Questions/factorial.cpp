c9d7e9ffe09f04d49f916f6f62ceee738fdde2e9
Abhishek Tilwar <abhishektilwar@gmail.com>
2021-05-19 21:37:31 +0530

    Update factorial.cpp

--- OOP Based Questions/factorial.cpp
+++ OOP Based Questions/factorial.cpp
@@ -14,3 +14,23 @@ int main(){
   cin>>n;
   cout<<fact(n)<<endl;              
 }
+
+// Code for nCr or combinational formula using factorial
+
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+    int factorial=1;
+    for(int i=1; i<=n; i++){
+        factorial*=i;
+    }
+    return factorial;
+}
+
+int main(){
+  int n,r;
+  cin>>n>>r;
+  cout<<fact(n)/(fact(n-r)*fact(r));
+               
+}
