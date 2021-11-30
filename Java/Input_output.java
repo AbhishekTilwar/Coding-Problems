4e3f16164f6b0bec269437811ba353d26899b06a
Abhishek Tilwar <abhishektilwar@gmail.com>
2021-11-30 23:48:29 +0530

    Update and rename OOP Based Problem to Input_output.java

---
+++ Java/Input_output.java
@@ -0,0 +1,16 @@
+
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        String name =sc.next();
+        System.out.println(name);
+        int a= sc.nextInt();
+        System.out.println(a);
+
+
+    }
+}
