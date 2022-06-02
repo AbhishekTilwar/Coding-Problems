7bcf3ae21efbd122024b294bb935e194ab933e42
Abhishek Tilwar <abhishektilwar@gmail.com>
2022-06-02 16:36:37 +0530

    Update Palindrome string.cpp

--- OOP Based Questions/Palindrome string.cpp
+++ OOP Based Questions/Palindrome string.cpp
@@ -36,3 +36,30 @@ int main(){
 
     return 0;
 }
+
+
+//Using Recursion
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool palindrome(string s, int i){
+    if(i>s.size()/2){
+        return true;
+    }
+     return s[i]==s[s.size()-i-1] && palindrome(s, i+1);
+}
+
+int main(){
+    //Palindrome
+    string s;
+    cin>>s;
+    if(palindrome(s,0)){
+        cout<<"Palindrome";
+    }
+    else{
+        cout<<"Non-Palindrome";
+    }
+}
+
